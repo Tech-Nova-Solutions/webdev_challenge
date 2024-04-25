40d42cb7a1ece86c811f8f79a2edd2d02daf523b
Giulia Barbizan <giuliabarbizan@gmail.com>
2024-04-24 22:11:01 -0300

    Criando função para alterar a cor de fundo

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let btnDarkLight = document.querySelector('#btnDarkLight');
 const email = 'admin@admin.com.br';
 const senha = 'admin';
 const credencialincorreta = document.querySelector('#credencialincorreta');
+let body = document.querySelector('body');
+
+
 
 btnLogin.addEventListener('click', () => {
     if(emailDigitado.value == email && senhaDigitada.value == senha){
@@ -16,4 +19,18 @@ btnLogin.addEventListener('click', () => {
         credencialincorreta.style.color = 'red';
     }
         
-} )
+})
+
+
+function mudarCor(){
+    function mudar(numero){
+        return(Math.random()* numero);
+    }
+    const cores= `rgb(${mudar(255)}, ${mudar(255)}, ${mudar(255)})`;
+    document.body.style.backgroundColor=cores;
+}
+    window.addEventListener('load',function(){
+        mudarCor();
+    }
+    )
+ 
