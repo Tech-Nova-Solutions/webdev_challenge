de0009af39a880b03f1e0d83799cdbbc11593a36
Leonardo <leonardorscarpitta@gmail.com>
2024-04-24 21:23:12 -0300

    Adicionando ID ao botão de login e declarando variaveis no JS

---
+++ script.js
@@ -0,0 +1,7 @@
+let nomeUsuario = prompt('Seja bem-vindo(a), qual seu nome?')
+let emailDigitado = document.querySelector('#email');
+let senhaDigitada = document.querySelector('#senha');
+let btnLogin = document.querySelector('#btnSubmit');
+let btnDarkLight = document.querySelector('#btnDarkLight');
+const email = 'admin@admin.com.br';
+const senha = 'admin';
