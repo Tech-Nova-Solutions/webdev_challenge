173540bd8fa1579aaa194c661748671d27b07988
Giulia Barbizan <giuliabarbizan@gmail.com>
2024-04-24 21:46:43 -0300

    Criando evento de validação de Login

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ let btnLogin = document.querySelector('#btnSubmit');
 let btnDarkLight = document.querySelector('#btnDarkLight');
 const email = 'admin@admin.com.br';
 const senha = 'admin';
+const credencialincorreta = document.querySelector('#credencialincorreta');
+
+btnLogin.addEventListener('click', () => {
+    if(emailDigitado.value == email && senhaDigitada.value == senha){
+        alert('ok');
+        credencialincorreta.innerHTML = '';
+    }else{
+        credencialincorreta.innerHTML = 'E-mail e/ou Senha incorretos';
+        credencialincorreta.style.color = 'red';
+    }
+        
+} )
